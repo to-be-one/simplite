c78e009ff7a9fe8961ba20f775de8663a724d913
zhangshaolong <369669902@qq.com>
2014-04-24 17:02:53 +0800

    合并解析标签处理

--- simplite/simplite.js
+++ simplite/simplite.js
@@ -16,9 +16,7 @@ var Simplite = function() {
     var isArray = isType('Array');
     /**
      * 获取dom元素
-     * 
-     * @private param {string|dom|jquery} idOrNode
-     *          支持domId，dom元素和jquery包装的dom元素方式
+     * @private param {string|dom|jquery} idOrNode 支持domId，dom元素和jquery包装的dom元素方式
      * @return {dom} 返回指定的dom元素
      */
     var getNode = function(idOrNode) {
@@ -32,9 +30,7 @@ var Simplite = function() {
     };
     /**
      * 获取指定dom元素的字符串模板
-     * 
-     * @private param {string|dom|jquery} idOrNodeOrHtml
-     *          支持domId，dom元素和jquery包装的dom元素方式或者html字符串
+     * @private param {string|dom|jquery} idOrNodeOrHtml 支持domId，dom元素和jquery包装的dom元素方式或者html字符串
      * @return {string} dom结构的字符串形式
      */
     var getTemplate = function(idOrNodeOrHtml) {
@@ -62,7 +58,6 @@ var Simplite = function() {
     };
     /**
      * 对html元素进行转义
-     * 
      * @private param {string} txt html字符串
      * @return {string} 返回转义好的html字符串
      */
@@ -79,7 +74,6 @@ var Simplite = function() {
     };
     /**
      * 解析开始标签
-     * 
      * @private param {string} text html模板
      * @return {array.string} 返回根据开始标签分割的数组
      */
@@ -88,7 +82,6 @@ var Simplite = function() {
     };
     /**
      * 解析闭合标签
-     * 
      * @private param {string} segment html模板段
      * @return {array.string} 返回根据闭合标签分割的数组
      */
@@ -96,78 +89,100 @@ var Simplite = function() {
         return segment.split(closeTag);
     };
     /**
-     * html模板解析
-     * 
-     * @private param {string} text html模板
-     * @return {string} 返回根据html模板转换为可运行js的字符串形式
+     * 自定义属性标签处理器
+     * @private param {string} attr 属性
+     * @private param {string} html 没有属性的html片段
+     * @return {string} 返回解析好的js代码段
      */
-    var parse = function(text) {
-        var out = 'var out = "";';
-        var segments = parseOpenTag(text, Simplite.logicOpenTag);
+    var customAttrHandler = function (attr, html) {
+        var out = '';
+        if (attr) {
+            if (attr.indexOf('#') === 0) {
+                out += 'out += Simplite.escapeHTML(' + attr.substr(1) + ');';
+            } else {
+                out += 'out += ' + attr + ';';
+            }
+        }
+        if (html) {
+            out += 'out += ' + stringify(html) + ';';
+        }
+        return out;
+    };
+    /**
+     * 逻辑标签处理器（当属性与逻辑使用相同标签时一起解析）
+     * @private param {string} js 代码逻辑或者属性
+     * @private param {string} html 可能包含自定义属性标签的html片段
+     * @return {string} 返回解析好的js代码段
+     */
+    var normalHandler = function (js, html) {
+        var out = '';
+        if (/^=\s*(.)/.test(js)) { // 是否是获取数据
+            if (RegExp.$1 === '#') { // 此数据需要html转义
+                js = 'out += Simplite.escapeHTML(' + js.substr(2) + ');';
+            } else {
+                js = 'out += ' + js.substr(1) + ';';
+            }
+        }
+        // 处理子模板，未处理xxx.include情况
+        js = js.replace(/(\W)?(include\s*\(([^\)]+)\))/g, function(all,
+                pre, include, args) {
+            if (args.indexOf(',') < 0) { // 不应该有第一个字符为“,”的情况。
+                args = args + ',_this';
+            }
+            return (pre || '') + 'out += Simplite.include(' + args + ')';
+        });
+        if (/[^\{;]\s*$/.test(js)) {
+            js += '\n'; // 为没有分号的情况添加换行，利用浏览器解析token
+        }
+        out += js;
+        // 处理自定义属性标签
+        if (html) {
+            if (Simplite.attrOpenTag !== Simplite.logicOpenTag
+                && Simplite.attrCloseTag !== Simplite.logicCloseTag
+            ) {
+                out += dealTag(html, Simplite.attrOpenTag,
+                    Simplite.attrCloseTag, customAttrHandler);
+            } else {
+                out += 'out += ' + stringify(html) + ';';
+            }
+        }
+        return out;
+    };
+    /**
+     * 模板逻辑和属性标签处理器
+     * @private param {string} text 处理文本
+     * @private param {string} openTag 开始标签
+     * @private param {string} openTag 闭合标签
+     * @private param {function} hanlder 具体根据解析生成语句段的处理器
+     * @return {string} 返回解析好的js代码段
+     */
+    var dealTag = function (text, openTag, closeTag, hanlder) {
+        var out = '';
+        var segments = parseOpenTag(text, openTag);
         for (var i = 0, len = segments.length; i < len; i++) {
             var segment = segments[i];
-            var jsAndHtml = parseCloseTag(segment, Simplite.logicCloseTag);
+            var jsAndHtml = parseCloseTag(segment, closeTag);
             var js = jsAndHtml[0];
             var html = jsAndHtml[1];
             if (html === undefined) {
                 html = js;
                 js = '';
             }
-            if (/^=\s*(.)/.test(js)) { // 是否是获取数据
-                if (RegExp.$1 === '#') { // 此数据需要html转义
-                    js = 'out += Simplite.escapeHTML(' + js.substr(2) + ');';
-                } else {
-                    js = 'out += ' + js.substr(1) + ';';
-                }
-            }
-            // 处理子模板，未处理xxx.include情况
-            js = js.replace(/(\W)?(include\s*\(([^\)]+)\))/g, function(all,
-                    pre, include, args) {
-                if (args.indexOf(',') < 0) { // 不应该有第一个字符为“,”的情况。
-                    args = args + ',_this';
-                }
-                return (pre || '') + 'out += Simplite.include(' + args + ')';
-            });
-            if (/[^\{;]\s*$/.test(js)) {
-                js += '\n'; // 为没有分号的情况添加换行，利用浏览器解析token
-            }
-            out += js;
-            // 处理自定义属性标签
-            if (html) {
-                if (Simplite.attrOpenTag !== Simplite.logicOpenTag
-                    && Simplite.attrCloseTag !== Simplite.logicCloseTag
-                ) {
-                    var htmlSegments = parseOpenTag(html, Simplite.attrOpenTag);
-                    for (var j = 0, l = htmlSegments.length; j < l; j++) {
-                        var htmlSegment = htmlSegments[j];
-                        var attrAndHtml = parseCloseTag(htmlSegment, Simplite.attrCloseTag);
-                        var attr = attrAndHtml[0];
-                        var html = attrAndHtml[1];
-                        if (html === undefined) {
-                            html = attr;
-                            attr = '';
-                        }
-                        if (attr) {
-                            if (attr.indexOf('#') === 0) {
-                                out += 'out += Simplite.escapeHTML(' + attr.substr(1) + ');';
-                            } else {
-                                out += 'out += ' + attr + ';';
-                            }
-                        }
-                        if (html) {
-                            out += 'out += ' + stringify(html) + ';';
-                        }
-                    }
-                } else {
-                    out += 'out += ' + stringify(html) + ';';
-                }
-            }
+            out += hanlder(js, html);
         }
         return out;
     };
     /**
+     * html模板解析
+     * @private param {string} text html模板
+     * @return {string} 返回根据html模板转换为可运行js的字符串形式
+     */
+    var parse = function(text) {
+        return  'var out = "";' + dealTag(text, Simplite.logicOpenTag,
+            Simplite.logicCloseTag, normalHandler);
+    };
+    /**
      * html模板编译
-     * 
      * @private
      * @return {function(string)} 返回根据html模板编译好的处理函数
      */
@@ -182,12 +197,9 @@ var Simplite = function() {
     };
     /**
      * 引入子模板
-     * 
      * @private
-     * @param {string|Dom|Jquery}
-     *            template 模板元素或者模板html
-     * @param {object}
-     *            data 用来填充模板的数据
+     * @param {string|Dom|Jquery} template 模板元素或者模板html
+     * @param {object} data 用来填充模板的数据
      * @return {string} 返回使用data数据填充好模板的html字符串
      */
     var include = function(template, data) {
@@ -196,12 +208,9 @@ var Simplite = function() {
     };
     /**
      * 向模板填充数据
-     * 
      * @private
-     * @param {string}
-     *            template 模板html
-     * @param {object}
-     *            data 用来填充模板的数据
+     * @param {string} template 模板html
+     * @param {object} data 用来填充模板的数据
      * @return {string} 返回使用data数据填充好模板的html字符串
      */
     var toHtml = function(template, data) {
@@ -209,14 +218,10 @@ var Simplite = function() {
     };
     /**
      * 根据字符串的html生成dom节点并添加到target元素
-     * 
      * @private
-     * @param {string|Dom|Jquery}
-     *            target html填充到的指定元素
-     * @param {string}
-     *            html 用来填充模板的已经填充好数据的字符串html
-     * @param {function(target)=}
-     *            fun 回调函数
+     * @param {string|Dom|Jquery} target html填充到的指定元素
+     * @param {string} html 用来填充模板的已经填充好数据的字符串html
+     * @param {function(target)=} fun 回调函数
      */
     var render = function(target, html, fun) {
         target = getNode(target);
@@ -226,15 +231,10 @@ var Simplite = function() {
 
     /**
      * 模板对象构造器，可以使用new初始化模板对象，也可以使用静态函数方式完成模板相关功能调用
-     * 
      * @constructor
-     * 
-     * @param {Object}
-     *            options 初始化配置参数
-     * @param {string|Dom|Jquery}
-     *            option.target 模板填充到的指定元素，支持domId，dom元素和jquery包装的dom元素方式
-     * @param {string|Dom|Jquery}
-     *            option.template 模板元素或模板内容
+     * @param {Object} options 初始化配置参数
+     * @param {string|Dom|Jquery} option.target 模板填充到的指定元素，支持domId，dom元素和jquery包装的dom元素方式
+     * @param {string|Dom|Jquery} option.template 模板元素或模板内容
      */
     var Simplite = function(options) {
         this.target = getNode(options.target);
@@ -242,7 +242,6 @@ var Simplite = function() {
     };
     /**
      * html模板编译
-     * 
      * @public
      * @return {function(object)} 返回根据html模板编译好的处理函数
      */
@@ -251,7 +250,6 @@ var Simplite = function() {
     };
     /**
      * 模板渲染到dom树
-     * 
      * @public
      * @return {function(object)} 返回根据html模板编译好的处理函数
      */
@@ -261,21 +259,17 @@ var Simplite = function() {
     };
     /**
      * 模板渲染前事件
-     * 
      * @public
      * @event
-     * @param {object}
-     *            用来渲染模板的数据
+     * @param {object} 用来渲染模板的数据
      */
     Simplite.prototype.beforerender = function(data) {
     };
     /**
      * 模板渲染后事件
-     * 
      * @public
      * @event
-     * @param {dom}
-     *            模板渲染到的dom元素
+     * @param {dom} 模板渲染到的dom元素
      */
     Simplite.prototype.afterrender = function(node) {
     };
